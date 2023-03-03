af6a3b365903fef4bcbbd165d3b8c3978976767c
Ritvik G <ritvikg@Ritviks-MacBook-Air.local>
2023-03-03 21:40:23 +0530

    react router changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-/*
+
 import { createRoot } from 'react-dom/client';
 import App  from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App/>);*/
+root.render(<App/>);
 
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+*/
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <HashRouter>
+      <App />
+    </HashRouter>
+);
+*/
