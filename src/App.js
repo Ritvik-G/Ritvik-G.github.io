a0628558f3d127dc1f42f9a3dc99834a48451a82
Ritvik G <ritvikg@Ritviks-MacBook-Air.local>
2023-03-03 21:55:57 +0530

    ending changes

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import About from './Pages/About/About';
 import Contact from './Pages/Contact/Contact';
 import Projects from './Pages/Projects/Projects';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
   <>
-    <Router>
+    <HashRouter basename='/'>
       <Navbar/>
       <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='contact' element={<Contact/>}/>
-          <Route path='about' element = {<About/>}/>
-          <Route path='projects' element = {<Projects/>}/>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/contact' element={<Contact/>}/>
+          <Route exact path='/about' element = {<About/>}/>
+          <Route exact path='/projects' element = {<Projects/>}/>
       </Routes>
       <Footer/>
-    </Router>
+    </HashRouter>
   </>
   );
 }
