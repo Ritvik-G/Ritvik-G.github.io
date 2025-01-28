f2c3f3f99b9014190a7028bf7fba42e767a19c76
Ritvik G <42266602+Ritvik-G@users.noreply.github.com>
2025-01-28 10:11:58 -0500

    Test

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
         <div className='profile-content'>
             <p>I am Ritvik G <br/></p>
             <p>An Inquisitive and Energetic Computer Science Undergrad with over 7 years of experience in programming.</p>
-            <p>I am passionate about creating digital software solutions to the real world problems that make our lives easier and better.</p>
+            <p>I am passionate about creating digital software solutions to the real world problems that make our lives easier and better. Tesin</p>
             <a href="#/about" style={{textDecoration: "none",color:"black",fontWeight:"bold"}}>Learn More &gt;</a>
         </div>
         
